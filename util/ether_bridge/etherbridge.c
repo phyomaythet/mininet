2c8041562c37a49206df9c723b5345e422152cfd
Baohua Yang <baohyang@cn.ibm.com>
2014-02-10 17:30:41 +0800

    reorg the code structure, using thread for unibidirectional mode too

--- util/ether_bridge/etherbridge.c
+++ util/ether_bridge/etherbridge.c
@@ -12,61 +12,28 @@
 #include <unistd.h>
 #include <pthread.h>
 
-#include "headers.h"
+#include "hdrs.h"
 
+int _DEBUG_=0;
 char *dev = NULL;						/* capture device1 name */
 char *dev2 = NULL;						/* capture device2 name */
 
-pcap_t *handle_dev=NULL;			    /* packet capture handle_dev */
-pcap_t *handle_dev2=NULL;				/* packet sending handle_dev2 */
+pcap_t *handler=NULL;			    /* packet capture handler */
+pcap_t *handler2=NULL;				/* packet sending handler2 */
 
-int hide_header = 0;
+int hide_hdr = 0;
 int hide_payload = 1;
 int capture_only = 1;
 int unidirectional = 0;
-int num_packets = -1;					/* number of packets to capture */
+int num_pkts = -1;					/* number of packets to capture */
 char *bpfFilter = NULL;				    /* filter expression */
 
 extern char *optarg;
 extern int optind, opterr;
 
-void got_packet_dev(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-void got_packet_dev2(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet, pcap_t *fwd_dev);
-
-/**
- * got packet at dev.
- */
-void got_packet_dev(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-    printf("got_packet_dev\n");
-    got_packet(args, header, packet,handle_dev2);
-}
-
-/**
- * got packet at dev2.
- */
-void got_packet_dev2(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-    printf("got_packet_dev2\n");
-    got_packet(args, header, packet,handle_dev);
-}
-/*
- * dissect/print packet
- */
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet,pcap_t *fwd_dev) {
-
-    static int count = 1;                   /* packet counter */
-    printf("Received pkt F#%i:\n",count);
-    if (parse_pkt(packet,hide_header,hide_payload)>=0) {
-        if (!capture_only && fwd_dev != NULL) {
-            send_packet(fwd_dev,packet,header->len);
-        }
-    }
-        
-
-
-    count++;
-    return;
-}
+void fwd_to_handler2(u_char *args, const struct pcap_pkthdr *hdr, const u_char *packet);
+void fwd_to_handler(u_char *args, const struct pcap_pkthdr *hdr, const u_char *packet);
+void fwd_packet(u_char *args, const struct pcap_pkthdr *hdr, const u_char *packet, pcap_t *fwd_dev);
 
 /**
  * Compile and apply the filter expression.
@@ -86,6 +53,9 @@ int set_filter(pcap_t *handle,struct bpf_program *fp,const char*bpfFilter){
     return 0;
 }
 
+/**
+ * open a pcap handler and init it.
+ */
 pcap_t * open_and_init(const char *dev_name, const char *bpfFilter) {
     pcap_t *handle = NULL;
     char errbuf[PCAP_ERRBUF_SIZE];			/* error buffer */
@@ -98,7 +68,7 @@ pcap_t * open_and_init(const char *dev_name, const char *bpfFilter) {
 
     /* make sure we're capturing on an Ethernet device */
     if (pcap_datalink(handle) != DLT_EN10MB) {
-        fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", dev_name);
+        fprintf(stderr, "Device %s doesn't provide Ethernet hdrs - not supported\n", dev_name);
         return NULL;
     }
 
@@ -112,21 +82,53 @@ pcap_t * open_and_init(const char *dev_name, const char *bpfFilter) {
     return handle;
 }
 
-void *cap_and_fwd(void *device_name) {
+/**
+ * got packet at dev, and try to fwd to dev2.
+ */
+void fwd_to_handler2(u_char *args, const struct pcap_pkthdr *hdr, const u_char *packet) {
+    if(_DEBUG_) printf("fwd_to_handler2\n");
+    fwd_packet(args, hdr, packet,handler2);
+}
+
+/**
+ * got packet at dev2, and try to fwd to dev.
+ */
+void fwd_to_handler(u_char *args, const struct pcap_pkthdr *hdr, const u_char *packet) {
+    if(_DEBUG_) printf("fwd_to_handler\n");
+    fwd_packet(args, hdr, packet,handler);
+}
+/**
+ * try to see if need to send out the given pkt. 
+ */
+void fwd_packet(u_char *args, const struct pcap_pkthdr *hdr, const u_char *packet,pcap_t *fwd_dev) {
+    if(_DEBUG_) {
+        static int count = 1;    /* packet counter */
+        printf("Received pkt F#%i:\n",count++);
+    }
+    if (parse_pkt(packet,hide_hdr,hide_payload)>=0) {
+        if (!capture_only && fwd_dev != NULL) {
+            send_packet(fwd_dev,packet,hdr->len);
+        }
+    }
+    return;
+}
+
+/**
+ * processing for a thread.
+ */
+void *got_pkt(void *device_name) {
     if(strcmp(device_name,dev)==0){
-        if(pcap_loop(handle_dev, num_packets, got_packet_dev, NULL)==-1) {
-             fprintf(stderr, "ERROR: %s\n", pcap_geterr(handle_dev));
+        if(pcap_loop(handler, num_pkts, fwd_to_handler2, NULL)==-1) {
+             fprintf(stderr, "ERROR: %s\n", pcap_geterr(handler));
              return NULL;
         }
-        if (handle_dev != NULL) pcap_close(handle_dev);
+        if (handler != NULL) pcap_close(handler);
     } else if(strcmp(device_name,dev2)==0){
-        if(unidirectional==0) {
-            if(pcap_loop(handle_dev2, num_packets, got_packet_dev2, NULL)==-1) {
-             fprintf(stderr, "ERROR: %s\n", pcap_geterr(handle_dev2));
-             return NULL;
-            }
-            if (handle_dev2 != NULL) pcap_close(handle_dev2);
+        if(pcap_loop(handler2, num_pkts, fwd_to_handler, NULL)==-1) {
+            fprintf(stderr, "ERROR: %s\n", pcap_geterr(handler2));
+            return NULL;
         }
+        if (handler2 != NULL) pcap_close(handler2);
     }
     return NULL;
 }
@@ -139,27 +141,27 @@ int main(int argc, char **argv) {
     while ((c = getopt(argc, argv, "i:I:n:uhpf:")) != EOF) {
         switch (c) {
             case 'i':
-                dev = optarg;
+                dev = optarg; //the capture device
                 dev2 = dev;	
                 break;
             case 'I':
-                dev2 = optarg;
+                dev2 = optarg; //the forward device
                 if(dev2 == dev){
-                    fprintf(stderr, "Should use differnet interface to forward\n");
+                    fprintf(stderr, "Should use different interface to forward\n");
                     return 0;
                 } else {
                     capture_only = 0;
                 }
                 break;
             case 'n':
-                num_packets = atoi(optarg);
+                num_pkts = atoi(optarg);
                 break;
             case 'f':
-                printf("num_packets= %d, dev2=%s\n", num_packets, dev2);
+                printf("num_pkts= %d, dev2=%s\n", num_pkts, dev2);
                 bpfFilter = strdup(optarg);
                 break;
             case 'h':
-                hide_header = 1;
+                hide_hdr = 1;
                 break;
             case 'p':
                 hide_payload = 1;
@@ -178,28 +180,32 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    if(num_packets > 0) printf("Packets to capture: %d\n", num_packets);
+    if(num_pkts > 0) printf("Packets to capture: %d\n", num_pkts);
 
-    handle_dev = open_and_init(dev,bpfFilter);
+    handler = open_and_init(dev,bpfFilter);
     if (!capture_only){
-        handle_dev2 = open_and_init(dev2,bpfFilter);
+        handler2 = open_and_init(dev2,bpfFilter);
     }
     if (!unidirectional) {
-        if (!capture_only) printf("Capture and forward between: %s, %s\n", dev, dev2);
-        pthread_create(&t1, NULL, cap_and_fwd, (void*) dev);
-        pthread_create(&t2, NULL, cap_and_fwd, (void*) dev2);
+        printf("Bidirectional process between: %s, %s\n", dev, dev2);
+        pthread_create(&t1, NULL, got_pkt, (void*) dev);
+        pthread_create(&t2, NULL, got_pkt, (void*) dev2);
         pthread_join(t1, NULL);
         pthread_join(t2, NULL);
     } else {
-        if (!capture_only) printf("Capture from %s, and forward to %s\n", dev, dev2);
-        if(pcap_loop(handle_dev, num_packets, got_packet_dev, NULL)==-1){
-            fprintf(stderr, "ERROR: %s\n", pcap_geterr(handle_dev));
+        printf("unidirectional process from %s to %s\n", dev, dev2);
+        pthread_create(&t1, NULL, got_pkt, (void*) dev);
+        pthread_join(t1, NULL);
+        /*
+        if(pcap_loop(handler, num_pkts, fwd_to_handler2, NULL)==-1){
+            fprintf(stderr, "ERROR: %s\n", pcap_geterr(handler));
             return 2;
         }
-        /* cleanup */
-        if (handle_dev != NULL) pcap_close(handle_dev);
-        if (handle_dev2 != NULL) pcap_close(handle_dev2);
+        */
     }
+    /* cleanup */
+    if (handler != NULL) pcap_close(handler);
+    if (handler2 != NULL) pcap_close(handler2);
     printf("\n\n--- Process complete. ---\n\n");
     return 0;
 }
